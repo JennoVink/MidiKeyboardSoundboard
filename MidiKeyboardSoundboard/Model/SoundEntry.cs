85f18c4d1484fce252b2cd9c8919eb1bad3a64ac
Jenno Vink <jenno-vink@hotmail.com>
2022-05-05 19:53:46 +0200

    Resharper cleanup, added a button flash animation timer. When you spam one button, the animation didn't work properly. That's wh a Timer is needed.

--- MidiKeyboardSoundboard/Model/SoundEntry.cs
+++ MidiKeyboardSoundboard/Model/SoundEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Timers;
 using System.Windows;
 using System.Windows.Media;
 using GalaSoft.MvvmLight;
@@ -9,30 +10,40 @@ namespace MidiKeyboardSoundboard.Model
 {
     public class SoundEntry : ObservableObject
     {
-        public Uri SoundPath { get; set; }
-
-        [JsonIgnore] public string FileName => Path.GetFileName(SoundPath.ToString());
-        [JsonIgnore] public MediaPlayer Player { get; set; }
-        [JsonIgnore] public bool IsPlaying { get; set; }
-
-        public bool LoopSound { get; set; }
-
-
         [JsonConstructor]
         public SoundEntry(Uri soundPath)
         {
             SoundPath = soundPath;
             Player = new MediaPlayer();
             Player.MediaEnded += Media_Ended;
+            ButtonFlashAnimationTimer = new Timer(100)
+            {
+                AutoReset = false
+            };
+            ButtonFlashAnimationTimer.Elapsed += (sender, args) =>
+            {
+                RaisePropertyChanged(nameof(ButtonFlashAnimationTimer));
+            };
         }
 
         public SoundEntry() : this(new Uri(@"c:\"))
         {
         }
 
+        public Uri SoundPath { get; set; }
+
+        [JsonIgnore] public string FileName => Path.GetFileName(SoundPath.ToString());
+        [JsonIgnore] public MediaPlayer Player { get; set; }
+        [JsonIgnore] public bool IsPlaying { get; set; }
+        [JsonIgnore] public Timer ButtonFlashAnimationTimer { get; set; }
+
+        public bool LoopSound { get; set; }
+
         public void Play()
         {
             IsPlaying = true;
+            ButtonFlashAnimationTimer.Start();
+            RaisePropertyChanged(nameof(ButtonFlashAnimationTimer));
             Application.Current.Dispatcher.Invoke(() =>
             {
                 Player.Open(SoundPath);
@@ -44,10 +55,7 @@ namespace MidiKeyboardSoundboard.Model
         {
             IsPlaying = false;
 
-            if (!LoopSound)
-            {
-                return;
-            }
+            if (!LoopSound) return;
 
             IsPlaying = true; // to trigger the flashing animation.
 
@@ -62,16 +70,12 @@ namespace MidiKeyboardSoundboard.Model
         }
 
         /// <summary>
-        /// Note that the volume has to be between 0 and 100.
+        ///     Note that the volume has to be between 0 and 100.
         /// </summary>
         /// <param name="volume"></param>
         public void SetVolume(int volume)
         {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                Player.Volume = volume / 100.0f;
-            });
+            Application.Current.Dispatcher.Invoke(() => { Player.Volume = volume / 100.0f; });
         }
-
     }
 }
